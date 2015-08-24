41e72450049f1a0a2e8fe9d0d681c41ef9c65485
Jordan Griffiths <jlg108@uclive.ac.nz>
2015-08-24 19:37:20 +1200

    Extended Deck class

--- core/src/com/jtsoftware/GameObjects/Deck.java
+++ core/src/com/jtsoftware/GameObjects/Deck.java
@@ -1,23 +1,33 @@
 package com.jtsoftware.GameObjects;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Jonty on 24/08/2015.
  */
 public class Deck {
-    enum Suite {CLUB, HEART, SPADE, DIAMOND}
-
+    enum Suit {CLUB, HEART, SPADE, DIAMOND}
+    Random numGen;
+    int remainingCards;
     ArrayList<Card> cards;
 
     public Deck(){
         cards = new ArrayList<Card>();
+        remainingCards = 52;
+        numGen = new Random();
 
         resetPack();
-
-
     }
 
+    public Card getCard() {
+        if (remainingCards < 1) {
+            return null;
+        }
+        int cardIndex = numGen.nextInt(this.remainingCards);
+        this.remainingCards -= 1;
+        return cards.remove(cardIndex);
+    }
 
     public void Shuffle(){
         //Do something maybe.
@@ -26,12 +36,10 @@ public class Deck {
     public void resetPack(){
         cards.clear();
 
-        for(Suite suite : Suite.values()){
+        for(Suit suite : Suit.values()){
 
             for(int i = 1; i < 15; i++){
                 cards.add(new Card(suite.name(), i));
-
-
             }
 
         }
